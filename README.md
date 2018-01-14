26e2d40ae6ea5c816b3cf7ec442b8a84e433bb08
vpascal <vpascal2@gmail.com>
2018-01-13 21:25:31 -0500

    cleaning code

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # sp_app
 Mockup app for organizing topics in various buckets based on the
 sortable lists (sortable.js)
+
+Features:
+	* drag and drop topics back and forth, into different ‘buckets’;
+	* drag and drop stuff between buckets; rearrange topics within each bucket;
+	* create new topics, delete them and sort them;
+	* create new buckets and delete them;
+
+To do:
+	* zoom in/out of bucket
+	* sort  entries within each bucket
+	* other small stuff
---
+++ app.js
@@ -0,0 +1,73 @@
+var editableList = Sortable.create(editable, {
+  group: 'shared',
+  animation: 100,
+  filter: '.js-remove',
+  onFilter: function (evt) {
+    var el = editableList.closest(evt.item);
+    el && el.parentNode.removeChild(el);
+  }
+});
+
+function myFunction(){
+
+    Ply.dialog('prompt', {
+      title: 'Add',
+      form: { name: 'Category' }
+    }).done(function (ui) {
+      var el = document.createElement('li');
+      el.innerHTML = ui.data.name + '<i class="js-remove">✖</i>';
+      editableList.el.appendChild(el);
+    });
+  }; 
+
+
+var counter = 4;
+
+function bucket() {
+    
+    var div = document.createElement('div');
+    div.setAttribute('class', "w3-container w3-quarter");
+    var localcounter = counter+1;
+    div.innerHTML="<h2>Bucket "+localcounter+"</h2><ul class='highlight w3-ul w3-border w3-hoverable w3-card-2' id=qux"+counter+"><li></li></ul><span onclick='this.parentElement.style.display=\x22none\x22' class='w3-button'>&times;</span>";
+    var x = 'qux'+ counter;
+    counter++;
+    document.getElementById("secondrow").appendChild(div);
+   //console.log(x);
+    Sortable.create(document.getElementById(x),{
+      group:'shared',
+      animation: 100
+    });
+
+     
+};
+
+function reset(id) {
+    var node = document.getElementById(id).childNodes;
+    //var node = document.getElementById("qux");
+    var list = document.getElementById("editable");
+    for (var i = 1; i < node.length; i++) {
+    list.insertBefore(node[i], list.childNodes[0]);
+  }
+};
+
+
+Sortable.create(qux, {
+  group: 'shared',
+  animation: 100
+});
+
+Sortable.create(qux1, {
+  group: 'shared',
+  animation: 100
+});
+
+Sortable.create(qux2, {
+  group: 'shared',
+  animation: 100
+});
+
+Sortable.create(qux3, {
+  group: 'shared',
+  animation: 100
+});
+
